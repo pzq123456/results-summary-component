df61292537fe8605d661d5dfecb800a50faaaf0b
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-03-24 21:37:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ResultsSummaryComponent from './components/Results-summary-component.vue';
+</script>
+
+<template>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- displays site properly based on user's device -->
+
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/images/favicon-32x32.png">
+    
+    <title>Frontend Mentor | Results summary component</title>
+    </head>
+
+    <body>
+        <div id="app">
+            <ResultsSummaryComponent />
+        </div>
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="#">Your Name Here</a>.
+        </div>
+    </body>
+</html>
+
+</template>
+
+<style>
+
+</style>
