a471396394ea04baac1a14edd699f88833c84649
潘志清 <82391775+pzq123456@users.noreply.github.com>
2023-03-25 20:34:05 +0800

    done!

--- src/components/RSCopnent.vue
+++ src/components/RSCopnent.vue
@@ -2,19 +2,31 @@
     <div class="card-wrapper">
         <div class="card">
             <div class="card-left">
-            <h2>Your Result</h2>
-            <h1>76</h1>
-            <h3>of 100</h3>
-            <h2>Great</h2>
-            <p>
-                You scored higher than 65% of the people who have taken these tests.
-            </p>
+
+                <div class="title">
+                    <h2>Your Result</h2>
+                </div>
+
+                <div class="circle">
+                    <h1>76</h1>
+                    <p>of 100</p>
+                </div>
+                <div class="commit">
+                    <h2>Great</h2>
+                    <p>
+                        You scored higher than 65% of the people who have taken these tests.
+                    </p>
+                </div>
+
+
+
+
             </div>
             <div class="card-right">
                 <h2>Summary</h2>
                 <div v-for=" (data, index ) in dataList" :key="data.category" class="dataBar" :style="colors[index]">
                     <!-- img with path of data.icon -->
-                    <div class="icon" :style="colors[index]">
+                    <div class="icon" >
                         <img :src=data.icon alt="icon">
                     </div>
                     <h3 :style="TextColors[index]">{{data.category}}</h3>
@@ -89,13 +101,13 @@ const TextColors = ref([
         justify-content: space-between;
         align-items: center;
         width: 100%;
-        max-width: 760px;
-        height: 500px;
+        max-width: 740px;
+        height: 520px;
         background-color: var(--neutral-white);
         border-radius: 30px;
-        box-shadow: 0px 0px 30px 0px rgba(50, 79, 238, 0.517);
+        box-shadow: 0px 0px 30px 0px rgba(50, 78, 238, 0.308);
     }
-
+/* left card */
     .card-left{
         width: 50%;
         height: 100%;
@@ -103,6 +115,15 @@ const TextColors = ref([
         /* vertical gradient bacground  var(--gradient-light-slate-blue) var(--gradient-light-royal-blue) */
         background: linear-gradient(180deg, var(--gradient-light-slate-blue) 0%, var(--gradient-light-royal-blue) 100%);
         border-radius: 30px;
+
+
+        /* flex */
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: start;
+
+
     }
 
     .card-right{
@@ -138,14 +159,6 @@ const TextColors = ref([
         font-weight: 700;
         margin: 0 0 0 20px;
     }
-
-    .card-left p{
-        color: var(--neutral-white);
-        font-size: 18px;
-        font-weight: 400;
-        margin: 20px 0 0 20px;
-    }
-
     .card-right h2{
         color: var(--neutral-dark-blue);
         font-size: 24px;
@@ -190,7 +203,7 @@ const TextColors = ref([
     }
 
     .card-right button{
-        width: 290px;
+        width: 300px;
         height: 55px;
 
         background-color: var(--neutral-dark-gray-blue);
@@ -207,10 +220,14 @@ const TextColors = ref([
         background-color: var(--gradient-light-royal-blue);
     }
 
+    /* align in right */
     .score-end{
         display: flex;
+        justify-content: flex-end;
         align-items: center;
-        margin: 0 0 0 40px;
+        width: 100%;
+        height: 100px;
+        margin: 0 40px 0 0;
     }
 
     .score-end .strong{
@@ -219,8 +236,116 @@ const TextColors = ref([
     }
 
     .score-end .weak{
-        font-weight: 400;
+        font-weight: 600;
+        color:rgba(11, 11, 11, 0.413);
     }
 
+
+    .title{
+
+        /* background-color: rgba(240, 248, 255, 0.471);  */
+        /* center */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        
+    }
+
+    .title h2{
+
+        font-size: 24px;
+        font-weight: 700;
+        color:var(--neutral-light-lavender)
+    }
+
+    .circle{
+        width: 180px;
+        height: 180px;
+        /* background-color: rgba(240, 248, 255, 0.609); */
+        border-radius: 50%;
+
+        /* align elemnts center and clumn*/
+        display: flex; 
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        /* center itself */
+        margin: 0 auto;
+
+        /* gradient background-color inverse against the left card */
+        background: linear-gradient(180deg, 
+        var(--gradient-violet-blue) 0%, 
+        var(--gradient-persian-blue) 100%);
+
+
+    }
+
+    .circle h1{
+        font-size: 80px;
+        font-weight: 600;
+        color:var(--neutral-white);
+    }
+
+    .circle p{
+        font-size: 16px;
+        font-weight: 600;
+        color:rgba(244, 242, 246, 0.365);
+    }
+
+    .commit{
+        width: 300px;
+        height: 150px;
+        /* background-color: rgba(240, 248, 255, 0.609); */
+
+        /* align elemnts center and clumn*/
+        display: flex; 
+        /* justify-content: center;
+        align-items: center; */
+        flex-direction: column;
+
+        /* center itself and up from the buttom*/
+        margin: 0 auto;
+        margin-bottom: 40px;
+
+        /* space betweent items */
+        gap: 20px;
+    }
+
+    .commit h2{
+        font-size: 32px;
+        font-weight: 600;
+        color:var(--neutral-white);
+        /*self cneter */
+        margin: 0 auto;
+
+    }
+
+    .commit p{
+        /* font-size: 18px;
+        font-weight: 400;
+        color:var(--neutral-light-lavender);
+        margin-bottom: 60px;
+
+        text-align:start; */
+
+        /* space between characters */
+        letter-spacing: 1px;
+
+        font-size: 0.75rem;
+        width: 200px;
+        font-weight: 600;
+        color: var(--neutral-light-lavender);
+
+        line-height: 1.5;
+        text-align: center;
+        /* background-color: rgba(240, 248, 255, 0.539); */
+
+        /*align self center */
+        margin: 0 auto;
+
+
+    }
 
 </style>
